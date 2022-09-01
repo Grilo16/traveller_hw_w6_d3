24856feaab24220cba69cba822482282b93d5a4e
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-09-01 01:44:46 +0100

    finished traveller

--- models/traveller.js
+++ models/traveller.js
@@ -3,27 +3,38 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map(journey => journey.startLocation)
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map(journey => journey.endLocation)
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport
+  });
 
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance
+  });
 
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((totalDistance, journey) => {
+    return totalDistance + journey.distance
+  }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  let journeyNames = this.journeys.map(journey => journey.transport)
+    return journeyNames.filter((transport, index) => {
+      return journeyNames.indexOf(transport) === index
+    });
 };
 
 
